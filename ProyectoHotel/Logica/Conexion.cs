10d23a403a47c3180049dca6efce9963f8f4fe91
Daniel Felipe Rayo Rodriguez <daniel.rayo@mail.escuelaing.edu.co>
2026-08-01 17:40:55 -0500

    Mover la cadena de conexion a Web.config

--- ProyectoHotel/Logica/Conexion.cs
+++ ProyectoHotel/Logica/Conexion.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
 
 namespace ProyectoHotel.Logica
 {
     public class Conexion
     {
-        public static string CN = "Data Source=.\\SQLEXPRESS;Initial Catalog=DB_HOTEL;Integrated Security=True";
+        /// <summary>
+        /// Direccion de la base de datos. NO se escribe aqui: se lee del Web.config
+        /// (seccion connectionStrings, entrada "ConexionHotel"), para poder cambiarla
+        /// sin recompilar el proyecto.
+        /// </summary>
+        public static string CN = ObtenerCadenaConexion();
+
+        private const string CadenaPorDefecto = "Data Source=.\\SQLEXPRESS;Initial Catalog=DB_HOTEL;Integrated Security=True";
+
+        private static string ObtenerCadenaConexion()
+        {
+            ConnectionStringSettings configuracion = ConfigurationManager.ConnectionStrings["ConexionHotel"];
+
+            // Si alguien borra o daña la seccion del Web.config, se usa el valor por defecto
+            // en vez de dejar caer la aplicacion con un error incomprensible.
+            if (configuracion == null || string.IsNullOrWhiteSpace(configuracion.ConnectionString))
+                return CadenaPorDefecto;
+
+            return configuracion.ConnectionString;
+        }
     }
 }
